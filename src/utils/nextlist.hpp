c6dd7d28e9f0f44296a93463a6b522326cb61567
TheEVolk <elektro-volk.ru>
2018-04-16 03:53:50 +0800

    base utils

---
+++ src/utils/nextlist.hpp
@@ -0,0 +1,92 @@
+#pragma once
+#include "lock.h"
+
+template<class Key, class Value>
+class Nextlist {
+private:
+	struct _L {
+		struct _L* next = nullptr;
+		Key key;
+		Value value;
+	};
+	_L* _data = nullptr;
+	LSECT _lock;
+	int _size = 0;
+public:
+	Nextlist<Key, Value>()
+	{
+		_lock = lock_new();
+	}
+
+	void lock() {
+		lock_lock(_lock);
+	}
+
+	void unlock() {
+		lock_unlock(_lock);
+	}
+
+	void push(Key key, Value value)
+	{
+		lock();
+		_L *el = new _L();
+		el->next = _data;
+		el->key = key;
+		el->value = value;
+		_data = el;
+		_size++;
+		unlock();
+	}
+
+	void pop(Key key)
+	{
+		lock();
+		_L *last = _data;
+		for (_L *el = _data; el != nullptr; el = el->next) {
+			if (key == el->key) {
+				last->next = el->next;
+				delete el;
+				_size--;
+				break;
+			}
+			last = el;
+		}
+		unlock();
+	}
+
+	bool exists(Key key)
+	{
+		lock();
+		for (_L *el = _data; el != nullptr; el = el->next) {
+			if (key == el->key) {
+				unlock();
+				return true;
+			}
+		}
+		unlock();
+		return false;
+	}
+
+	Value& operator [] (Key key)
+	{
+		lock();
+		for (_L *el = _data; el != nullptr; el = el->next) {
+			if (key == el->key) {
+				unlock();
+				return el->value;
+			}
+		}
+
+		unlock();
+		throw "This key not found";
+	}
+
+	int size() {
+		return _size;
+	}
+
+	_L* data()
+	{
+		return _data;
+	}
+};
---
+++ src/utils/speedlist.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include "lock.h"
+
+template<class T>
+class Speedlist {
+private:
+	LSECT _lock;
+	int _size = 0;
+public:
+	T* _data = nullptr;
+
+	Speedlist<T>()
+	{
+		_lock = lock_new();
+	}
+
+	void lock() {
+		lock_lock(_lock);
+	}
+
+	void unlock() {
+		lock_unlock(_lock);
+	}
+
+	void clear() {
+		free(_data);
+		_data = nullptr;
+		_size = 0;
+	}
+
+	void push(T element)
+	{
+		lock();
+		if (_size == 0)
+			_data = (T*)malloc(sizeof(T));
+		else
+			_data = (T*)realloc(_data, (_size + 1) * sizeof(T));
+		_data[_size] = element;
+		_size++;
+		unlock();
+	}
+
+	void pop(T element)
+	{
+		lock();
+		T *newArray = (T*)malloc((_size - 1) * sizeof(T));
+		int newI = 0;
+		for (int i = 0; i < _size; i++)
+			if (_data[i] != element) {
+				newArray[newI] = _data[i];
+				newI++;
+			}
+		free(_data);
+		_data = newArray;
+		_size--;
+		unlock();
+	}
+
+	T& operator [] (int i)
+	{
+		return _data[i];
+	}
+	
+	int size() {
+		return _size;
+	}
+};
